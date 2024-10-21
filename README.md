6f5181c2595120c90322682e9a190e578447749b
Siddharth Gautam <siddharthkushwaha33@gmail.com>
2024-10-21 20:27:53 +0530

    Added a Template File to automate Project Structure

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-#### First Project 
+## First Project 
 
 ### Lap-1 : "setup.py" was created! 
 First we have Created the Setup.py file , requirements.txt, README.md and src files.
->  -e . is used in the end of the list of requirements.txt to 
->  include all the files in the current directory alaso we can build
->  it will automatically trigger the setup.py file and build the package 
+-e . is used in the end of the list of requirements.txt to 
+include all the files in the current directory alaso we can build
+it will automatically trigger the setup.py file and build the package 
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages ,setup
 from typing import List
 
+HYPEN_E_DOT = '-e .'
 
 def get_requirements(file_path:str)->List[str]:
     """
@@ -10,7 +11,9 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
-
+        # this is added after '-e .' was added in requirements.txt
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 setup(
     name = "data-science project",
     version= "0.0.1" ,
